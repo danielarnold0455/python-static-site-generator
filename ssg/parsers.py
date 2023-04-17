d58d064b9bc135dfdf7633a7a815f1c067ca8584
Daniel Arnold <daniel.arnold@ansys.com>
2023-04-17 13:48:18 +0200

    Parser(5)

---
+++ ssg/parsers.py
@@ -0,0 +1,23 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+
+    def __init__(self, lst: List[str]):
+        self.extensions = lst
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path, source, dest):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, mode='r') as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext='html'):
+        full_path = self.dest / path.with_suffix(ext).name
+
+
