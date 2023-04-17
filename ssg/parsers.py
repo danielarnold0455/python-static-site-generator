3beb8d1fb78b997ef7d8807f90bb0ce9b8dbcbd9
Daniel Arnold <daniel.arnold@ansys.com>
2023-04-17 13:55:29 +0200

    Parser(5)

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -12,10 +12,12 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
-        with open(path, mode='r') as file:
+        with open(path, 'r') as file:
             return file.read()
 
     def write(self, path, dest, content, ext='html'):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, 'w') as file:
+            file.write(content)
 
 
